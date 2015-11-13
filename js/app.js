6c48e71172bdda203068050262f8bcf399925cfa
DereK Han <derekhan206@gmail.com>
2015-11-13 11:16:53 -0800

    Adds form, button, angular module and controller

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 /**
  * Created by dhan206 on 11/13/15.
  */
+
+angular.module("SignUpApp" , ['ngSanitize'])
+
+    .controller("SignUpCtrl", ['$scope', function($scope) {
+
+        $scope.submitForm = function() {
+            $scope.
+        };
+    }]);
