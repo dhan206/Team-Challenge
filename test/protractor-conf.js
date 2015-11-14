258e16ee5affec97ea524e12fdc5be18e6b502e3
DereK Han <derekhan206@gmail.com>
2015-11-13 16:35:14 -0800

    Adds testing

--- test/protractor-conf.js
+++ test/protractor-conf.js
@@ -2,6 +2,7 @@
  * Created by dhan206 on 11/13/15.
  */
 
-describe("", function() {
-
-});
+exports.config = {
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['*signup-spec.js']
+};
--- test/signup-spec.js
+++ test/signup-spec.js
@@ -1,3 +1,20 @@
 /**
  * Created by dhan206 on 11/13/15.
  */
+
+'use strict';
+
+describe("Button feature", function() {
+
+    beforeEach(function () {
+        //navigate browser to the given url
+        browser.get("http://localhost:8000");
+    });
+
+    it("should disable the button if there form is invalid", function() {
+        var button = element(by.id("submitButton"));
+
+        expect(button.isEnabled()).toEqual(false);
+    });
+
+});
